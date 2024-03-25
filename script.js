a67f87134a02f471b3a6fe1caf03414a087d2ddf
unknown <okuliczadrian@gmail.com>
2024-03-25 18:03:20 +0100

    jd

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let koronka2 = "git2.png"
 let koronka3 = "git3.png"
 
 divy = document.querySelectorAll("div")
-console.log(divy.length)
+// console.log(divy.length)
 
 
 function loadyn(){
@@ -19,9 +19,10 @@ function hide_loadyn(){
     }
 }
 
-function zrob_tabele_whuj(){
+function zrob_tabele_whuj(a){
     const table = document.createElement('table');
     table.setAttribute('id', 'speedrunTable');
+    if (a==0){
     table.innerHTML = `
         <tr>
             <th>Miejsce</th>
@@ -31,6 +32,19 @@ function zrob_tabele_whuj(){
             <th>Top</th>
         </tr>
     `;
+    }
+    else{
+        table.innerHTML = `
+        <tr>
+            <th>Miejsce</th>
+            <th>Grajek</th>
+            <th>Czas</th>
+            <th>Cykl</th>
+            <th>Filmik</th>
+            <th>Top</th>
+        </tr>
+    `;
+    }
     return table
 }
 
@@ -59,6 +73,57 @@ async function collectData(apiUrl) {
                 var video = "Null";
             }
 
+            // console.log(data.data.category)
+            try {
+                var cyklson = data.data.runs[i].run.values;
+                var cykl = cyklson["68kokwqn"];
+
+                if (typeof cykl !== "undefined"){
+                }
+                else{
+                    var cykl = cyklson["onvvmx0n"];
+                }
+                // console.log(cykl)
+                switch (cykl){
+                    case "810yj92l":
+                        real_cykl = "-1";
+                        break;
+                    case "9qjone3l":
+                        real_cykl = "0";
+                        break;
+                    case "jq6wrgjl":
+                        real_cykl = "1";
+                        break
+                    case "5lme6wmq":
+                        real_cykl = "2";
+                        break
+                    case "81wjo3vq":
+                        real_cykl = "3+"
+                        break
+                    case "5q8n7g3q":
+                        real_cykl = "-1"
+                        break;
+                    case "4qyken21":
+                        real_cykl = "0"
+                        break;
+                    case "mlng65j1":
+                        real_cykl = "1"
+                        break;
+                    case "8102ex2q":
+                        real_cykl = "2"
+                        break
+                    case "9qjpyg3q":
+                        real_cykl = "3+"
+                        break
+
+                    
+                }
+                // console.log(real_cykl)
+            
+            } catch (error) {
+                var real_cykl = "Null";
+            }
+
             try {
                 var krajGrajka = (data.data.players.data[i].location.country.names.international);
             } catch(error) {
@@ -72,10 +137,12 @@ async function collectData(apiUrl) {
             }
 
             if (krajGrajka == "Poland") {
+
                 const formattedData = {
                     place: jd++,
                     player: grajek,
                     time: `${minutes}m ${sekundes}s ${milliseconds}ms`,
+                    wind: real_cykl,
                     videoLink: video,
                     top: i + 1
                 };
@@ -95,18 +162,19 @@ async function fetchSpeedrunData() {
         document.body.style.backgroundImage = "url('jk2.png')";
         loadyn();
         
-        const apiUrl = 'https://www.speedrun.com/api/v1/leaderboards/268ekxy6/category/n2y7rvz2?embed=players';
+        const apiUrl = 'https://www.speedrun.com/api/v1/leaderboards/268ekxy6/category/n2y7rvz2?embed=players,variables';
         const collectedData = await collectData(apiUrl);
 
-        const table = zrob_tabele_whuj();
+        const table = zrob_tabele_whuj(1);
 
         collectedData.forEach((rowData, index) => {
-            const { place, player, time, videoLink, top } = rowData;
+            const { place, player, time, wind, videoLink, top } = rowData;
             const row = table.insertRow(-1);
             row.innerHTML = `
                 <td>${place}</td>
                 <td>${player}</td>
                 <td>${time}</td>
+                <td>${wind}</td>
                 <td><a href="${videoLink}" target="_blank">link</a></td>
                 <td>${top}</td>
             `;
@@ -128,18 +196,19 @@ async function fetchSpeedrunData_NewBabePlus() {
         document.body.style.backgroundImage = "url('jk3.png')";
         loadyn();
         
-        const apiUrl = 'https://www.speedrun.com/api/v1/leaderboards/268ekxy6/category/9d8x83q2?embed=players';
+        const apiUrl = 'https://www.speedrun.com/api/v1/leaderboards/268ekxy6/category/9d8x83q2?embed=players,variables';
         const collectedData = await collectData(apiUrl);
 
-        const table = zrob_tabele_whuj();
+        const table = zrob_tabele_whuj(1);
 
         collectedData.forEach((rowData, index) => {
-            const { place, player, time, videoLink, top } = rowData;
+            const { place, player, time, wind, videoLink, top } = rowData;
             const row = table.insertRow(-1);
             row.innerHTML = `
                 <td>${place}</td>
                 <td>${player}</td>
                 <td>${time}</td>
+                <td>${wind}</td>
                 <td><a href="${videoLink}" target="_blank">link</a></td>
                 <td>${top}</td>
             `;
@@ -166,7 +235,7 @@ async function fetchSpeedrunData_gotb() {
         const apiUrl = 'https://www.speedrun.com/api/v1/leaderboards/268ekxy6/category/mke9q6jd?embed=players';
         const collectedData = await collectData(apiUrl);
 
-        const table = zrob_tabele_whuj();
+        const table = zrob_tabele_whuj(0);
 
         collectedData.forEach((rowData, index) => {
             const { place, player, time, videoLink, top } = rowData;
